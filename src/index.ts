fbda7c9db2466cf0f7dea620800caf8ea870a5f9
yangjiyoung <0112jyoung@naver.com>
2022-11-16 01:05:29 +0900

    [INIT] 초기 세팅

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-// ! 기본 세팅
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express(); 
+const PORT = 3000; 
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("초기 세팅 등록");
+});
+
+app.listen(PORT, () => {
+    console.log(`
+        #############################################
+            🛡️ Server listening on port: ${PORT} 🛡️
+        #############################################
+    `);
+}); 
