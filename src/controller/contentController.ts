937f2af2dca2db9096852ad597ae49e684fe6b5d
yangjiyoung <0112jyoung@naver.com>
2022-11-18 16:10:52 +0900

    [FEAT] 날짜 형식 추가

--- src/controller/contentController.ts
+++ src/controller/contentController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { contentService } from "../service";
 
+const userKeyRegExp=/^[0-9]{4}\-[0-9]{2}\-[0-9]{2}?$/;
+
 //메인 페이지 전체 일정 가져오기 
 const allContent  = async (req:Request, res:Response)=>{
     const data = await contentService.allContent();
@@ -20,7 +22,12 @@ const getAllContent = async (req:Request, res:Response)=>{
 //캘린더 페이지 해당 날짜 일정 가져오기
 const getDayContent = async(req:Request, res:Response)=>{
     const selectedDate: string =req.query.date as string;
-    
+    if(!selectedDate)
+        return res.status(404).json({ status: 404, message: "캘린더 페이지 해당 일정 가져오기 실패" });
+
+    if(!userKeyRegExp.test(selectedDate))
+        return res.status(404).json({ status: 404, message: "캘린더 페이지 해당 일정 가져오기 실패 - 잘못된 날짜 형식" });
+        
     const data= await contentService.getDayContent(selectedDate);
     
     return res.status(200).json({ status: 200, message: "캘린더 페이지 해당 일정 가져오기  성공", data });
