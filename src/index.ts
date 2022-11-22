a28de9c80aae1c59375baab98ed2c90044513174
gahee99 <u1879@sookmyung.ac.kr>
2022-11-22 19:50:03 +0900

    [ADD] cors에러 모듈 추가

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
-
+const cors = require('cors');
 const app = express(); 
 const PORT = 3000; 
 
 app.use(express.json());
 
-
+app.use(cors({
+    origin: '*', // 모든 출처 허용 옵션. true 를 써도 된다.
+}));
 app.use("/", router); // use -> 모든 요청
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
