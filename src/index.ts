2e2b222d0ca8c827314adc46f29fc5b4376dd6b7
yangjiyoung <0112jyoung@naver.com>
2022-11-22 19:50:08 +0900

    [FEAT] cors 적용

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
+import cors from "cors";
 
 const app = express(); 
 const PORT = 3000; 
 
 app.use(express.json());
-
+app.use(cors());
 
 app.use("/", router); // use -> 모든 요청
 
